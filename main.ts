16bfd3a5762e99bf6b76dc25e0127695a58321b2
Michael McCracken <michael.mccracken@gmail.com>
2023-06-09 16:35:50 -0700

    switch to configurable multi filters
    mostly work on settings.
    
    currently just two in defaults, no way to add more
    
    Signed-off-by: Michael McCracken <michael.mccracken@gmail.com>

--- main.ts
+++ main.ts
@@ -1,47 +1,78 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 import { execSync } from "child_process";
-//import { Convert } from "@rburns/ansi-to-html";
 
 import {
     default as AnsiUp
 } from 'ansi_up';
 
-interface SlogBlockSettings {
+interface AllFilterSettings {
+	filters: FilterArray;
+}
+
+
+interface FilterSettings {
+	codeblocklabel: string;
 	binpath: string;
+	colorize: boolean;
+	outputfont: string;
+	firstlineextraargs: string; // TODO use first line as extra args if available
 }
 
-const DEFAULT_SETTINGS: SlogBlockSettings = {
-	binpath: 'hl'
+interface FilterArray extends Array<FilterSettings> {}
+
+const SLOG = "slog"
+
+const DEFAULT_SETTINGS: AllFilterSettings = {
+	"filters": [{
+		codeblocklabel: SLOG,
+		binpath: 'hl',
+		colorize: true,
+		outputfont: 'IBM Plex Mono',
+	},
+				{
+		codeblocklabel: "haha",
+		binpath: 'hl',
+		colorize: false,
+		outputfont: 'Comic Sans',
+	}]
 }
 
-export default class SlogBlock extends Plugin {
-	settings: SlogBlockSettings;
+export default class FilterBlock extends Plugin {
+	settings: AllFilterSettings;
 
 	async onload() {
 		await this.loadSettings();
 
+		for (let fidx = 0; fidx < this.settings.filters.length; fidx++) {
+			let filter	= this.settings.filters[fidx]
+			this.registerMarkdownCodeBlockProcessor(filter.codeblocklabel, (source, el, ctx) => {
 
-    this.registerMarkdownCodeBlockProcessor("slog", (source, el, ctx) => {
-		const dest = document.createElement('div')
-   		const opts = {input: source, timeout: 60000}
-		const output = execSync(this.settings.binpath + " -", opts)
-    const ansi_up = new AnsiUp();
+				const dest = document.createElement('div')
+				try{
+   					const opts = {input: source, timeout: 60000}
+					const output = execSync(filter.binpath + " -", opts)
+					const ansi_up = new AnsiUp();
 
-		const rows = output.toString().split("\n")
-      for (let i = 0; i < rows.length; i++) {
-		  let htmlized = ansi_up.ansi_to_html(rows[i]);
-          let line = dest.createEl("div")
-		  line.innerHTML = htmlized
-		  line.style.fontFamily = "monospace"
-		  line.style.font = "IBM Plex Mono"
-       }
-		el.replaceWith(dest)
-    });
+					const rows = output.toString().split("\n")
+					for (let i = 0; i < rows.length; i++) {
+						let htmlized = rows[i]
+						if (filter.colorize){
+							htmlized = ansi_up.ansi_to_html(htmlized)
+						}
+						let line = dest.createEl("div")
+						line.innerHTML = htmlized
+						line.style.fontFamily = "monospace"
+						line.style.font = filter.outputfont
+					}
+				}catch (e){
+					dest.createDiv(e.message)
+				}
+				el.replaceWith(dest)
+			});
+		}
 
-	//   This adds a settings tab so the user can configure various aspects of the plugin
-	this.addSettingTab(new SlogBlockSettingTab(this.app, this));
+		this.addSettingTab(new FilterSettingTab(this.app, this));
 
-			// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
@@ -59,10 +90,10 @@ export default class SlogBlock extends Plugin {
 }
 
 
-class SlogBlockSettingTab extends PluginSettingTab {
-	plugin: SlogBlock;
+class FilterSettingTab extends PluginSettingTab {
+	plugin: FilterBlock;
 
-	constructor(app: App, plugin: SlogBlock) {
+	constructor(app: App, plugin: FilterBlock) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -72,18 +103,58 @@ class SlogBlockSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for structured logging rewriting.'});
+		containerEl.createEl('h1', {text: 'Settings for filtering code blocks through local cmdline tools.'});
 
-		new Setting(containerEl)
-			.setName('filter binary')
-			.setDesc('path to binary to filter the code block')
-			.addText(text => text
-				.setPlaceholder('hl')
-				.setValue(this.plugin.settings.binpath)
+		for (let i = 0; i < this.plugin.settings.filters.length; i++){
+
+			let filterSetting = this.plugin.settings.filters[i]
+
+			containerEl.createEl('h2', {text: 'Filter'});
+
+			new Setting(containerEl)
+				.setName('code block label')
+				.setDesc('label you add to code block to run this filter')
+				.addText(text => text
+				.setPlaceholder('label')
+				.setValue(filterSetting.codeblocklabel)
 				.onChange(async (value) => {
-					console.log('bin path: ' + value);
-					this.plugin.settings.binpath = value;
+					filterSetting.codeblocklabel = value;
 					await this.plugin.saveSettings();
 				}));
+
+			new Setting(containerEl)
+				.setName('filter binary')
+				.setDesc('path and args to binary to filter the code block')
+				.addText(text => text
+				.setPlaceholder('path to binary and any cmdline args')
+				.setValue(filterSetting.binpath)
+				.onChange(async (value) => {
+					filterSetting.binpath = value;
+					await this.plugin.saveSettings();
+				}));
+
+			new Setting(containerEl)
+				.setName('colorize')
+				.setDesc('should we try to convert ansi color escapes')
+				.addToggle(toggle => toggle
+				.setValue(filterSetting.colorize)
+				.onChange(async (value) => {
+					filterSetting.colorize = value;
+					await this.plugin.saveSettings();
+				}));
+
+			new Setting(containerEl)
+				.setName('output font')
+				.setDesc('font name to use for output')
+				.addText(text => text
+				.setPlaceholder('font name')
+				.setValue(filterSetting.outputfont)
+				.onChange(async (value) => {
+					filterSetting.outputfont = value;
+					await this.plugin.saveSettings();
+				}));
+			// todo add a button to remove the filter
+		}
+		// todo: add a button to add a filter
 	}
 }
